db1ac55bd1195ca70a2d5f63800e8a5ef1cc46ff
nicosampler <nf.dominguez.87@gmail.com>
2019-08-30 11:02:12 -0300

    apollo conections and code reorg

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Subgraph - starter kit</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import App from './containers/App'
+import { client } from './apolloClientConfig'
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App networkInfo={{ network: process.env.REACT_APP_GRAPH_NETWORK }} />
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
